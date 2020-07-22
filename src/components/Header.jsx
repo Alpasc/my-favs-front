936bf60dc3ef3e5abbe2eb1eb9d8f2672af44ce2
Alpasc <alinepascal@gmail.com>
2020-07-22 23:16:07 +0200

    mise en place de la page d'accueil

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class header extends Component {
+  render() {
+    return (
+      <div>
+        <div className='entete'>
+          <h1>My Fav's</h1>
+          <h2>Gestion de favoris multimédias</h2>
+        </div>
+      </div>
+    )
+  }
+}
