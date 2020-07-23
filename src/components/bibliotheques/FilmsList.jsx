efc46508f4277404651b40b3dd3d1dc0abf61d72
Alpasc <alinepascal@gmail.com>
2020-07-23 11:51:42 +0200

    affichage des films de la bdd ok

--- src/components/bibliotheques/FilmsList.jsx
+++ src/components/bibliotheques/FilmsList.jsx
@@ -1,11 +1,56 @@
 import React, { Component } from 'react';
-import '../../App.css';
+import axios from 'axios';
+// import Footer from '../Footer';
 
-export default class FilmsList extends Component {
+export default class Film extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      films: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('/film')
+      .then(response => response.data)
+      .then(film => {
+        this.setState({
+          films: film.results
+        });
+      });
+  }
+
   render() {
+    const { films } = this.state;
     return (
       <div>
-        <h1>Mes films</h1>
+      <div className="movie entete">
+        <h1>Mes Films</h1>
+      </div>
+      <div className='tabTitle'>
+        <h2>Ma Filmothèque</h2>
+      </div>
+      <table className='movieTable'>
+        <tr>
+          <th className='tabEntry'>Titre</th>
+          <th className='tabEntry'>Réalisateur</th>
+          <th className='tabEntry'>Acteurs</th>
+          <th className='tabEntry'>Genre</th>
+          <th className='tabEntry'>Supprimer</th>
+        </tr>
+
+        {films.map(film => (
+          <tr className='sectionTab'>
+            <td className='filmTab' key={film.id}>{film.nom}</td>
+            <td className='filmTab' key={film.id}>{film.realisateur}</td>
+            <td className='filmTab' key={film.id}>{film.acteurs}</td>
+            <td className='filmTab' key={film.id}>{film.genre}</td>
+            <td className='filmTab'><button>X</button></td>
+          </tr>
+        ))}
+      </table>
+      {/* <Footer /> */}
       </div>
     )
   }
