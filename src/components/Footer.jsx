97bde00edc8c0efdecb86bff9823d662d919ee4f
Alpasc <alinepascal@gmail.com>
2020-07-27 14:50:44 +0200

    amelioration affichage page accueil

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,10 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
   render() {
     return (
-      <div>
-        <hr className='lign' />
       <footer className="finPage">
         <p className='signature'><a href="https://github.com/Alpasc" target="blank" className="txtDeco">Créé par  Alpasc</a></p>
         <p className="year">2020</p>
       </footer>
-      </div>
     )
   }
 }
